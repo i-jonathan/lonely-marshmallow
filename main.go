31e339a250548d9b5cad88b1fcb824cd06ea77f1
Jonathan <farinloyejonathan@gmail.com>
2021-08-06 07:12:38 +0100

    fixed port

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	bot.SetHandler(handler)
 
 	log.Println("Starting Server")
-	err = http.ListenAndServe(":7901", http.HandlerFunc(bot.UpdateHandler))
+	err = http.ListenAndServe(":"+os.Getenv("PORT"), http.HandlerFunc(bot.UpdateHandler))
 
 	if err != nil {
 		log.Println("Failed")
